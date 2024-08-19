202a198dbc820572f53083cbe0d78ad93497b63c
Anh Pham Hoang <harrispham.dev@gmail.com>
2024-08-19 15:40:09 +0700

    first commit

---
+++ src/main/java/com/hcmute/prse_be/PrseBeApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.prse_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrseBeApplication.class, args);
+    }
+
+}
