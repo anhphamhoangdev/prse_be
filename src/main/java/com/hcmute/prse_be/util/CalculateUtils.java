88a3ead4a558a43998a9fa53d5b18eb6bd2456db
Anh Pham Hoang <harrispham.dev@gmail.com>
2024-12-14 21:05:39 +0700

    fix bug : growth rate

--- src/main/java/com/hcmute/prse_be/util/CalculateUtils.java
+++ src/main/java/com/hcmute/prse_be/util/CalculateUtils.java
@@ -3,7 +3,8 @@ package com.hcmute.prse_be.util;
 public class CalculateUtils {
     public static double calculateGrowthRate(double current, double previous) {
         // If previous is 0, set base to 1 to avoid division by zero
-        double base = (previous == 0) ? 1.0 : previous;
-        return ConvertUtils.toDouble((current - base) / base * 100);
+//        double base = (previous == 0) ? 1.0 : previous;
+//        return ConvertUtils.toDouble((current - base) / base * 100);
+        return ((previous == 0) ? 0.0 : (current - previous) / previous * 100);
     }
 }
